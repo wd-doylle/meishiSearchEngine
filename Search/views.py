3275953a45f877205aabf94037232f6541b2d39b
wd-doylle <18221943430@163.com>
2018-01-13 09:51:20 +0800

    changes

--- Search/views.py
+++ Search/views.py
@@ -88,8 +88,8 @@ def booleanSearch(request):
     else:
         return HttpResponseRedirect('/')
 
-ingred_s = open('Search/ingre_sort.txt')
-name_s = open('Search/name_sort.txt')
+ingred_s = open('ingre_sort.txt')
+name_s = open('name_sort.txt')
 ingredients = [l.split()[0].decode('utf-8') for l in ingred_s.readlines()]
 names = [l.split()[0] for l in name_s.readlines()]
 ingred_s.close()
--- meishi/views.py
+++ meishi/views.py
@@ -46,8 +46,8 @@ def clickCount(request):
     else:
         return HttpResponseRedirect('/')
 
-ingred_s = open('Search/ingre_sort.txt')
-name_s = open('Search/name_sort.txt')
+ingred_s = open('ingre_sort.txt')
+name_s = open('name_sort.txt')
 ingredients = [l.split()[0].decode('utf-8') for l in ingred_s.readlines()]
 names = [l.split()[0] for l in name_s.readlines()]
 random.seed(time.time())
